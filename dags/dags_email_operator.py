e326727e341373fc9d38311bb462ff84ec697f6a
MSRhie <stat_12@naver.com>
2025-10-29 15:32:57 +0900

    emil operator 수정

--- dags/dags_email_operator.py
+++ dags/dags_email_operator.py
@@ -5,13 +5,13 @@ from airflow.providers.smtp.operators.smtp import EmailOperator
 
 with DAG(
     dag_id="dags_email_operator",
-    schedule="0 8 1 * *",
+    schedule=None,
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
     send_email_task = EmailOperator(
         task_id='send_email_task',
-        conn_id='conn_smtp_gmail',      # Airflow 3.0 실습부터 추가
+        conn_id='conn_smtp_gmail',      # Airflow 3.0 실습부터 추가 # localhost:8080 에서 smtp 연결 설정 필요
         to='nanki1004@gmail.com',       # 본인의 메일 계정으로 변경해주세요.
         subject='Airflow 성공메일',
         html_content='Airflow 작업이 완료되었습니다'
