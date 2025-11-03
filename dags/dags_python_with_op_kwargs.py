9bfb68c707a82320758b5049155118a4c25f4f05
MSRhie <nanki1004@gmail.com>
2025-11-03 14:43:45 +0900

    op_kwargs 파일 업로드

---
+++ dags/dags_python_with_op_kwargs.py
@@ -0,0 +1,22 @@
+import pendulum
+# Airflow 3.0 부터 아래 경로로 import 합니다.
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.sdk import task
+from common.common_func import regist2
+
+with DAG(
+    dag_id="dags_python_with_op_kwargs",
+    schedule="0 2 * * 1",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    regist_t1 = PythonOperator(
+        task_id='regist_t1',
+        python_callable=regist2,
+        op_args=['hjkim', 'man', 'kr', 'seoul'],
+        op_kwargs={'email': 'hjkim_sun@naver.com', 'phone': '010'}
+    )
+
+    regist_t1
