bc328a1c5d293c094bb3bb0451ee0352cd3c1f9f
MSRhie <nanki1004@gmail.com>
2025-11-04 11:51:54 +0900

    data interval 테스트

---
+++ dags/dags_cron_trigger_timetable.py
@@ -0,0 +1,27 @@
+from airflow.timetables.trigger import CronTriggerTimetable
+from airflow.sdk import DAG, task
+from datetime import timedelta
+from pprint import pprint
+
+with DAG(
+    dag_id='dags_cron_trigger_timetable',
+    schedule=CronTriggerTimetable(
+        cron='*/5 * * * *',
+        timezone='Asia/Seoul',
+        interval=timedelta(minutes=5)
+    ),
+    tags=['cron']
+) as dag:
+    """
+    Config Parmeter create_cron_date_intervals = True를 지정하더라도
+    CronTriggerTimetable을 사용하게 되면 Interval을고려하지 않게됨
+    즉 data_interval_start/end 컨텍스트 변순느 모두 동일한 값(DAG실행된 날짜)를 가지게 됨
+    """
+    
+    @task(task_id='task_show_context1')
+    def task_show_context1(**context):
+        print('::group::show context variables')
+        pprint(context)
+        print('::endgroup::')
+    
+    task_show_context1()
